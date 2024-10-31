5f98fc6c48ada6d91566804b6c4d95d452a1945f
teh0393 <teh0393@utulsa.edu>
2024-10-31 08:46:15 -0500

    finished question 2

--- Problem 2/driving.py
+++ Problem 2/driving.py
@@ -1,10 +1,12 @@
 def driving_cost(miles_per_gallon, dollars_per_gallon, miles_driven):
-   #write your code here
-   return
+   return(dollars_per_gallon*miles_driven/miles_per_gallon)
 
 if __name__ == '__main__':
     miles_per_gallon = float(input())
     dollars_per_gallon = float(input())
-    #print your costs here like example below
-    #print(f'{driving_cost(miles_per_gallon, dollars_per_gallon, 10.0):.2f}')
-   
+    cost_to_miles=driving_cost(miles_per_gallon, dollars_per_gallon, 10)
+    print(f'{cost_to_miles:.2f}')
+    cost_to_miles=driving_cost(miles_per_gallon, dollars_per_gallon, 50)
+    print(f'{cost_to_miles:.2f}')
+    cost_to_miles=driving_cost(miles_per_gallon, dollars_per_gallon, 400)
+    print(f'{cost_to_miles:.2f}')
