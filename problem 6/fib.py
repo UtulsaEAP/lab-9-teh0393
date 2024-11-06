4c55d346350197fe6c07c1e067764242e6deaa16
teh0393 <teh0393@utulsa.edu>
2024-11-06 17:42:26 -0600

    Finished problem 6

--- problem 6/fib.py
+++ problem 6/fib.py
@@ -1,7 +1,9 @@
 def fibonacci(n):
-    
-    #write your code here
-    return
+    sequence=[0,1]
+    while len(sequence) < n+1:
+        sequence.append(sequence[-1] + sequence[-2])
+    return sequence[n]
+
 
 if __name__ == '__main__':
     start_num = int(input())
